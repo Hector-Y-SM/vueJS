7181438cfd5a0de1419a8c31ebd7fb6e55f2e3cc
Hector-Y-SM <hm96012@gmail.com>
2025-08-30 10:12:21 -0700

    first project

---
+++ hola-mundo/README.md
@@ -0,0 +1,1 @@
+# A to-do app built with Vue.js to learn the basics.
---
+++ hola-mundo/src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+  import { isSpreadAssignment } from 'typescript';
+import { ref } from 'vue';
+  const goals = ref([]);
+  const inputGoalValue = ref('');
+
+
+  const addGoal = () => {
+    goals.value.push(inputGoalValue.value);
+  }
+
+  const removeGoal = (index) => {
+    console.log(index)
+    goals.value.splice(index, 1);
+  }
+
+</script>
+
+<template>
+  <header>
+    <h1 class="text-center text-3xl uppercase font-bold bg-green-300 p-6">vue course goal</h1>
+  </header>
+  <section class="flex flex-col justify-center m-20 p-4 rounded-2xl">
+    <h2 class="text-center text-2xl font-medium mb-10">my course</h2>
+    <input type="text" 
+           v-model="inputGoalValue"
+           placeholder="escribeeee" 
+           class="p-2 bg-gray-300 w-full" 
+    >
+    <button @click="addGoal"
+            class="w-full p-3 bg-green-500 rounded-b-md">add goals</button>
+      <p v-if="goals.length === 0"
+         class="text-2xl text-red-400 text-center" 
+      > 
+        no goals
+      </p>
+    <ul v-else 
+        class="font-bold text-4xl text-center"
+    >
+      <li v-for="(goal, index) in goals"
+      >
+        {{ index + 1}} - {{goal}}
+        <button 
+          @click="removeGoal(index)"
+          class="p-3 bg-red-500"
+        >borrar</button>
+      </li>
+    </ul>
+  </section>
+
+</template>
+
